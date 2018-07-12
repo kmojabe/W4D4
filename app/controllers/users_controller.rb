7b384c50a0c3ad8a7e070167e0019f68c4429466
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-12 16:12:39 -0700

    Warmup Complete pt. 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     render :index
   end
   
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+  
   def new
     @user = User.new
     render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create, :show]
   
   resources :sessions, only: [:new, :create, :destroy]
 
+  
 
 end
 
