207e810c2882dec6dae9669aef3add40b5d25688
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-12 11:42:26 -0700

    UserController and User Model created as well as routes and schema

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index 
+    @user = User.new
+    render :index
+  end
+  
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    @user.password = params[:password]
+    if @user.save
+      redirect_to users_url
+    else
+      flash[:errors] = ['Email/Password combination did not create a new user']
+      redirect_to new_user
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).allow(:email, :password)
+  end
+  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |_|
+  temp = User.new(email: Faker::Internet.email)
+  temp.password = Faker::Internet.password(6,10)
+  temp.save!
+end
